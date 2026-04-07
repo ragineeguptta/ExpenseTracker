65d508fa4ab3d19d4e871876a705e173932b0bee
Raginee Gupta <ragineegupta123@gmail.com>
2026-04-07 20:23:15 +0530

    Add project files.

---
+++ ExpenseTracker.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ExpenseTracker.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
